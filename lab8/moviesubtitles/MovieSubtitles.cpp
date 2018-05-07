39cae91a4ddde90ab2404651ae9328073b221f2f
naplet <39011649+naplet@users.noreply.github.com>
2018-05-07 12:27:36 +0200

    Update MovieSubtitles.cpp

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -10,11 +10,10 @@ namespace moviesubs {
         if (std::atoi(matches[1].str().c_str()) + Fps < 0) throw NegativeFrameAfterShift(LineIndex,line);
     }
 
-    void MicroDvdSubtitles::ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second, std::istream *in,
-                                                std::ostream *out) {
-
+    void MicroDvdSubtitles::ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second, std::istream *in, std::ostream *out) {
+    
         if(frame_per_second < 0) throw std::invalid_argument("Negative fps")
-        int Fps = (int)(((double)offset_in_micro_seconds/1000) * frame_per_second)
+        int Fps = (offset_in_micro_seconds*frame_per_second)/1000
         regex pattern {R"(\{(\d+)\}\{(\d+)\}(.+))"};
         int LineIndex = 1;
         int start = -1;
