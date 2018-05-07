20316089384c7667c0106a074ca4b361ed686c59
naplet <39011649+naplet@users.noreply.github.com>
2018-05-07 12:11:45 +0200

    Update MovieSubtitles.cpp

--- lab8/moviesubtitles/MovieSubtitles.cpp
+++ lab8/moviesubtitles/MovieSubtitles.cpp
@@ -1,70 +1,151 @@
 #include "MovieSubtitles.h"
 
-void moviesubs::MicroDvdSubtitles::ShiftAllSubtitlesBy(int delay, int framerate,
-                                                        std::stringstream* in, std::stringstream* out) {
 
-    if (framerate < 0){
-        throw std::invalid_argument("Negative framerate");
+namespace moviesubs {
+    void CheckLineMicroDVD(const string &line, regex pattern, int LineIndex, int Fps, int PreviousFrameStartTime){
+        smatch matches;
+        if (!std::regex_match(line, matches, pattern)) throw InvalidSubtitleLineFormat(LineIndex,line);
+        if (std::atoi(matches[1].str().c_str()) < PreviousFrameStartTime) throw OutOfOrderFrames(LineIndex, line);
+        if (std::atoi(matches[1].str().c_str()) > std::atoi(matches[2].str().c_str())) throw SubtitleEndBeforeStart(LineIndex, line);
+        if (std::atoi(matches[1].str().c_str()) + Fps < 0) throw NegativeFrameAfterShift(LineIndex,line);
     }
 
-    int shift_by=(delay*framerate)/1000;
+    void MicroDvdSubtitles::ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second, std::istream *in,
+                                                std::ostream *out) {
 
-    std::regex sub_line_regex(R"(\{(-?\d+)\}\{(-?\d+)\}(.+))");
-    int line_index=1;
-
-    char line_buffer[1000];
-
-    while(in->getline(line_buffer, 1000)) {
-        std::cmatch sub_line;
-        if (std::regex_match(line_buffer, sub_line, sub_line_regex)){
-            int start_frame = std::stoi(sub_line[1])+shift_by;
-            int end_frame = std::stoi(sub_line[2])+shift_by;
-            if (start_frame>end_frame){
-                throw(SubtitleEndBeforeStart(sub_line[0], line_index));
-            }
-            if (start_frame < 0){
-                throw(NegativeFrameAfterShift(sub_line[0], line_index));
-            }
-            (*out)<<"{"<<start_frame<<"}{"<<end_frame<<"}"<<sub_line[3]<<"\n";
-        } else{
-            throw(InvalidSubtitleLineFormat(std::string(line_buffer), line_index));
+        if(frame_per_second < 0) throw std::invalid_argument("Negative fps");
+        double miliseconds = offset_in_micro_seconds;
+        double calculate = (miliseconds/1000) * frame_per_second;
+        int Fps = (int) calculate;
+        regex pattern {R"(\{(\d+)\}\{(\d+)\}(.+))"};
+        int LineIndex = 1;
+        int start = -1;
+        while (!in->eof()) {
+            string buffor;
+            smatch matches;
+            std::getline(*in,buffor);
+            if(buffor == "") break;
+            CheckLineMicroDVD(buffor, pattern, LineIndex, Fps, start);
+            std::regex_match(buffor, matches, pattern);
+            start = std::atoi(matches[1].str().c_str());
+            *out << "{" << std::atoi(matches[1].str().c_str()) + Fps << "}{" <<
+                 std::atoi(matches[2].str().c_str()) + Fps << "}" << matches[3].str() << "\n";
+            LineIndex++;
         }
-        ++line_index;
+
+
     }
-}
+
+    bool SubRipSubtitles::IsEndGreater(const string &start, const string &end) {
+        if(std::atoi(end.substr(0,2).c_str()) < std::atoi(start.substr(0,2).c_str())) return false;
+        else if (std::atoi(end.substr(0,2).c_str()) == std::atoi(start.substr(0,2).c_str())){
+            if (std::atoi(end.substr(3,2).c_str()) < std::atoi(start.substr(3,2).c_str())) return false;
+            else if (std::atoi(end.substr(3,2).c_str()) == std::atoi(start.substr(3,2).c_str())){
+               if (std::atoi(end.substr(6,2).c_str()) < std::atoi(start.substr(6,2).c_str())) return false;
+               else if (std::atoi(end.substr(6,2).c_str()) == std::atoi(start.substr(6,2).c_str())){
+                   if (std::atoi(end.substr(9,3).c_str()) < std::atoi(start.substr(9,3).c_str())) return false;
+               }
+            }
+        }
+        return true;
+    }
+
+    void SubRipSubtitles::ValidateSubRip(const string &sub, regex pattern, int offset, int previousFrame){
+        smatch matches;
+        if(!std::regex_match(sub, matches ,pattern)) throw InvalidSubtitleLineFormat(1, sub);
+        if(previousFrame + 1 != std::atoi(matches[1].str().c_str())) throw OutOfOrderFrames(1, matches[1].str());
+        if(!IsEndGreater(matches[2].str(), matches[3].str())) throw SubtitleEndBeforeStart(2, matches[2].str() + " --> " + matches[3].str());
+        if(AddShift(offset, matches[2].str())[0] == '-') throw NegativeFrameAfterShift(1, sub);
+    }
+
+    void SubRipSubtitles::ShiftAllSubtitlesBy(int offset_in_micro_seconds, int frame_per_second, std::istream *in,
+                                              std::ostream *out) {
+
+        if(frame_per_second < 0) throw std::invalid_argument("Negative fps");
+        regex pattern {R"((\d+)\n(\d{2}\:\d{2}\:\d{2},\d{3}) --> (\d{2}\:\d{2}\:\d{2},\d{3})\n((?:.+\n*)+))"};
+        regex pattern2 {R"(\n\n)"};
+        smatch matches1;
+        string entireFile;
+        string line;
+        int previousFrame = 0;
+        while (!in->eof()){
+            std::getline(*in, line);
+            entireFile.append(line + "\n");
+        }
+        while (std::regex_search(entireFile, matches1, pattern2)){
+            smatch matches2;
+            string sub = matches1.prefix().str();
+            ValidateSubRip(sub, pattern, offset_in_micro_seconds, previousFrame);
+            std::regex_match(sub , matches2, pattern);
+            *out<<matches2[1].str()<<"\n"<<AddShift(offset_in_micro_seconds, matches2[2].str())<<" --> "<<AddShift(offset_in_micro_seconds, matches2[3].str())<<"\n"<<matches2[4].str()<<"\n\n";
+            entireFile = matches1.suffix().str();
+            previousFrame++;
+        }
+
+    }
+
+    string SubRipSubtitles::AddShift(int offset, const string &time) {
+        bool negative = false;
+        int hours = std::atoi(time.substr(0,2).c_str());
+        int minutes = std::atoi(time.substr(3,2).c_str());
+        int seconds = std::atoi(time.substr(6,2).c_str());
+        int mili = std::atoi(time.substr(9,3).c_str());
+        mili += offset;
+        while (mili >= 1000) {
+            mili -= 1000;
+            seconds += 1;
+            while (seconds >=60){
+                seconds -=60;
+                minutes += 1;
+                while (minutes >=60){
+                    minutes -=60;
+                    hours += 1;
+                }
+            }
+        }
+        while (mili <0) {
+            mili += 1000;
+            seconds -= 1;
+            while (seconds < 0){
+                seconds +=60;
+                minutes -= 1;
+                while (minutes < 0){
+                    minutes +=60;
+                    hours -= 1;
+                }
+                if(hours < 0) negative = true;
+            }
+        }
+        string result;
+        if (negative) result = "-";
+        vector <int> times {hours, minutes, seconds};
+        for (auto i : times){
+            if (i<10) result.append("0" + std::to_string(i) + ":");
+            else result.append(std::to_string(i) + ":");
+        }
+        if (mili < 10) result = result.substr(0,result.size() - 1) + ",00" + std::to_string(mili);
+        else if (mili < 100) result = result.substr(0,result.size() - 1) + ",0" + std::to_string(mili);
+        else result = result.substr(0,result.size() - 1) + "," + std::to_string(mili);
+        return result;
+
+    }
 
 
-moviesubs::SubtitleException::SubtitleException(const std::string &line_content, int line_number): std::invalid_argument{"SubtitleException"}{
-    std::stringstream out_stream;
-    line_number_=line_number;
-    out_stream<<"At line "<<line_number_<<": "<<line_content;
-    message_=out_stream.str();
-}
+    string SubtitlesException::prepare(int number) {
+        return " " +  std::to_string(number) + ": ";
+    }
 
-int moviesubs::SubtitleException::LineAt() const {
-    return line_number_;
-}
+    SubtitlesException::SubtitlesException(int lineNumber, const string &line) : std::invalid_argument("At line" + prepare(lineNumber) + line) {
+        lineNumber_ = lineNumber;
+    }
 
-const char *moviesubs::SubtitleException::what() const noexcept {
-    return message_.c_str();
-}
+    int SubtitlesException::LineAt() const {
+        return lineNumber_;
+    }
 
-moviesubs::NegativeFrameAfterShift::NegativeFrameAfterShift(const std::string &line_content, int line_number)
-        : SubtitleException(line_content, line_number) {
-
-}
-
-moviesubs::SubtitleEndBeforeStart::SubtitleEndBeforeStart(const std::string &line_content, int line_number)
-        : SubtitleException(line_content, line_number) {
-
-}
-
-moviesubs::InvalidSubtitleLineFormat::InvalidSubtitleLineFormat(const std::string &line_content, int line_number)
-        : SubtitleException(line_content, line_number) {
-
-}
-
-moviesubs::OutOfOrderFrames::OutOfOrderFrames(const std::string &line_content, int line_number) : SubtitleException(
-        line_content, line_number) {
-
+    NegativeFrameAfterShift::NegativeFrameAfterShift(int lineNumber, const string &line) : SubtitlesException(lineNumber, line) {}
+    InvalidSubtitleLineFormat::InvalidSubtitleLineFormat(int lineNumber, const string &line) : SubtitlesException(lineNumber, line) {}
+    OutOfOrderFrames::OutOfOrderFrames(int lineNumber, const string &line) : SubtitlesException(lineNumber, line) {}
+    SubtitleEndBeforeStart::SubtitleEndBeforeStart(int lineNumber, const string &line) : SubtitlesException(lineNumber, line) {}
+    MissingTimeSpecification::MissingTimeSpecification(int lineNumber, const string &line) : SubtitlesException(lineNumber, line) {}
 }
